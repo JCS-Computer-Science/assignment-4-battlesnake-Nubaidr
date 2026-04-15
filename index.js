c11ba2b626dc816b05d2fad17eb95e9aa1f33b9d
Nubaidr <nubaidr1@educbe.ca>
2026-04-15 16:49:00 +0000

    22

--- index.js
+++ index.js
@@ -12,33 +12,62 @@
 import express from "express";
 import move from "./moveLogic.js";
 
+
 const app = express();
 app.use(express.json());
 const config = {
-	apiversion: "1",
-	author: "", // TODO: Your Battlesnake Username
-	color: "#8d44c2", // TODO: Choose color
-	head: "default", // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
-	tail: "default", // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
+    apiversion: "1",
+    author: "retep", // TODO: Your Battlesnake Username
+    color: "#15ff00", // TODO: Choose color
+    head: "nr-rocket", // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
+    tail: "replit-notmark", // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
 };
 
+
 //TODO: respond to GET requests on "/" with the config object above
+app.get("/", (req, res) => {
+    res.json(config);
+});
+
 
 //TODO: respond to POST requests on "/start". Your response itself is ignored, but must have status code "200"
 //      the request body will contain objects representing the game instance, game board state, and your snake
 //      https://docs.battlesnake.com/api/requests/start
 
+
+// Game start
+app.post("/start", (req, res) => {
+    res.sendStatus(200);
+});
+
+
 //TODO: respond to POST requests on "/move". Your response should be an object with a "move" property and optionally
 //      a "shout" property. The request body again contains objects representing the game state
 //      https://docs.battlesnake.com/api/requests/move
+app.post("/move", (req, res) => {
+    res.json(move(req.body));
+});
+
+
+
 
 //TODO: respond to POST requests on "/end", which signals the end of a game. Your response itself is ignored,
 //      but must have status code "200" the request body will contain objects representing the game
 //      https://docs.battlesnake.com/api/requests/end
+app.post("/end", (req, res) => {
+    res.sendStatus(200);
+});
+
+
+
 
 const host = "0.0.0.0";
 const port = process.env.PORT || 8000;
 
+
 app.listen(port, host, () => {
-	console.log(`Running Battlesnake at http://${host}:${port}...`);
+    console.log(`Running Battlesnake at http://${host}:${port}...`);
 });
+
+
+
--- moveLogic.js
+++ moveLogic.js
@@ -6,9 +6,11 @@ export default function move(gameState){
         right: true
     };
     
-    // We've included code to prevent your Battlesnake from moving backwards
+
     const myHead = gameState.you.body[0];
     const myNeck = gameState.you.body[1];
+    const boardWidth = gameState.board.width;
+    const boardHeight = gameState.board.height
     
     if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
         moveSafety.left = false;
@@ -27,32 +29,119 @@ export default function move(gameState){
     // gameState.board contains an object representing the game board including its width and height
     // https://docs.battlesnake.com/api/objects/board
     
+    if (myHead.x === 0) moveSafety.left = false;
+    if (myHead.x === boardWidth - 1) moveSafety.right = false;
+    if (myHead.y === 0) moveSafety.down = false;
+    if (myHead.y === boardHeight - 1) moveSafety.up = false;
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
     
+    const myBody = gameState.you.body;
+    const bodyToCheck = myBody.slice(1, myBody.length - 1);
+    for (const segment of myBody){
+        if (segment.x === myHead.x - 1 && segment.y === myHead.y) moveSafety.left = false;
+        if (segment.x === myHead.x + 1 && segment.y === myHead.y) moveSafety.right = false;
+        if (segment.x === myHead.y - 1 && segment.x === myHead.x) moveSafety.down = false;
+        if (segment.x === myHead.y + 1 && segment.x === myHead.x) moveSafety.up = false;
+
+    }
+
+
     
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
     
     // Are there any safe moves left?
+
+    const opponents = gameState.board.snakes;
+    for (const snake of gameState.board.snakes) {
+        for (const segment of snake.body.slice(0, -1)) {
+            if (segment.x === myHead.x - 1 && segment.y === myHead.y) moveSafety.left = false;
+            if (segment.x === myHead.x + 1 && segment.y === myHead.y) moveSafety.right = false;
+            if (segment.y === myHead.y - 1 && segment.x === myHead.x) moveSafety.down = false;
+            if (segment.y === myHead.y + 1 && segment.x === myHead.x) moveSafety.up = false;
+        }
+    }
+
+    const enemyHeads = gameState.board.snakes.map(s => s.body[0]);
+    for (const head of enemyHeads) {
+        if (head.x === myHead.x - 1 && head.y === myHead.y) moveSafety.left = false;
+        if (head.x === myHead.x + 1 && head.y === myHead.y) moveSafety.right = false;
+        if (head.y === myHead.y - 1 && head.x === myHead.x) moveSafety.down = false;
+        if (head.y === myHead.y + 1 && head.x === myHead.x) moveSafety.up = false;
+    }
     
     //Object.keys(moveSafety) returns ["up", "down", "left", "right"]
     //.filter() filters the array based on the function provided as an argument (using arrow function syntax here)
     //In this case we want to filter out any of these directions for which moveSafety[direction] == false
     const safeMoves = Object.keys(moveSafety).filter(direction => moveSafety[direction]);
     if (safeMoves.length == 0) {
-        console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
-        return { move: "down" };
+        console.log(`MOVE ${gameState.turn}: No safe moves`);
+        return { move: "up" };
     }
     
     // Choose a random move from the safe moves
-    const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+    const nextMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];   
+    const food = gameState.board.food;
+    let bestMove = safeMoves[Math.floor(Math.random() * safeMoves.length)];
+
+    if (food.length > 0) {
+       
+        let nearestFood = food.reduce((closest, f) => {
+            const distance = Math.abs(f.x - myHead.x) + Math.abs(f.y - myHead.y);
+            if (distance < closest.distance) {
+                return { food: f, distance: distance };
+            }
+            return closest;
+        
+        
+
+
     
-    // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    // gameState.board.food contains an array of food coordinates https://docs.battlesnake.com/api/objects/board
-    
-    console.log(`MOVE ${gameState.turn}: ${nextMove}`)
-    return { move: nextMove };
+        let minDistance = Infinity;
+
+        for (const f of food) {
+            const distance = Math.abs(f.x - myHead.x) + Math.abs(f.y - myHead.y);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestFood = f;
+            }
+        }
+
+        for (const f of food) {
+            const distance = Math.abs(f.x - myHead.x) + Math.abs(f.y - myHead.y);
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestFood = f;
+            }
+        }
+
+
+    let bestDistance = Infinity;
+        const moveOffsets = {
+            up:    { x: 0,  y: 1  },
+            down:  { x: 0,  y: -1 },
+            left:  { x: -1, y: 0  },
+            right: { x: 1,  y: 0  }
+        };
+
+        for (const move of safeMoves) {
+            const nextX = myHead.x + moveOffsets[move].x;
+            const nextY = myHead.y + moveOffsets[move].y;
+            const distToFood = Math.abs(nearestFood.x - nextX) + Math.abs(nearestFood.y - nextY);
+
+            if (distToFood < bestDistance) {
+                bestDistance = distToFood;
+                bestMove = move;
+            }
+        }
+    }
 }
+
+    console.log(`MOVE ${gameState.turn}: ${bestMove}`);
+    return { move: bestMove };
+}
+
+
